266d9ba9b8d52a20c0884cfead9709b0343d7397
Anna Sibova <anna_sibova@ukr.net>
2023-04-22 16:45:38 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOption';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+  countTotalFeedback = totalFeedback => {
+    const { good, neutral, bad } = this.state;
+    totalFeedback = Number(good + neutral + bad);
+    return totalFeedback;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return Math.round((good / total) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+
+    return (
+      <>
+        <Section title="please leave feedback">
+          <FeedbackOptions
+            onLeaveFeedback={this.onLeaveFeedback}
+            options={Object.keys(this.state)}
+          ></FeedbackOptions>
+        </Section>
+
+        <Section title="statistics">
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            ></Statistics>
+          ) : (
+            <Notification message={'There is no feedback !'}></Notification>
+          )}
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOption.js
@@ -0,0 +1,28 @@
+import css from './FeedbackOption.module.css';
+import PropTypes from 'prop-types';
+
+const FeedbackOption = ({ options, onLeaveFeedback }) => (
+  <div className={css.FeedbackOptions}>
+    {options.map(option => {
+      return (
+        <button
+          className={css.feedbackOptions__btn}
+          key={option}
+          name={option}
+          onClick={() => {
+            onLeaveFeedback(option);
+          }}
+        >
+          {option}
+        </button>
+      );
+    })}
+  </div>
+);
+
+FeedbackOption.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOption;
